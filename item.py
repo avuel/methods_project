d4278b7625a9715ad4aa77d4420ec5d18faf6709
Jacob White <jake.andrew.white@gmail.com>
2021-11-25 10:27:37 -0600

    Added some documentatino for the Item Class.

--- item.py
+++ item.py
@@ -1,4 +1,6 @@
+# Item Class
 class Item:
+    # Constructor for the Item Class
     def __init__(self, itemId: int, name: str, unitCost: float, category: str, stock: int) -> None:
         self.__itemID: int = None
         self.__name: str = name
@@ -6,15 +8,18 @@ class Item:
         self.__category: str = category
         self.__stock: int = stock
 
+    # Get the Category of an Item
     def getCategory(self) -> str:
         return self.__category
 
+    # Get the Unit Cost of an Item
     def getUnitCost(self) -> float:
         return self.__unitCost
 
+    # Get the Stock of an Item
     def getStock(self) -> int:
         return self.__stock
 
+    # Set the Stock of an Item
     def setStock(self, stock: int) -> None:
         self.__stock = stock
-        
