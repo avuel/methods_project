6eb615bb7bc654d30b388fdf694d0330aba99e11
avuel <jake.andrew.white@gmail.com>
2021-12-01 14:17:48 -0600

    Revert "Merge pull request #22 from avuel/Customer"
    This reverts commit d107c9e4e00da502b41b8615b731b0e8b4712b87, reversing
    changes made to 64df260c52586de674d94ea7625ffb137427be07.

--- Customer.py
+++ Customer.py
@@ -2,7 +2,6 @@ from db import *
 
 class Customer:
 
-    #Parameterized constructor
     def __init__(self, username, password):
 
         self.__username = username
@@ -10,125 +9,25 @@ class Customer:
         self.__address = ""
         self.__city = ""
         self.__state = ""
-        self.__zipCode = ""
+        self.__zip = ""
         self.__paymentInfo = ""
         self.__orderHistory = []
 
-    # * These are the getters and setters for the Customer class *
-
-    def setUsername(self, username):
-        # Establish connection with the db and create a cursor
-        connect = sqlite3.connect("e-commerce.db")
-        cur = connect.cursor()
-
-        # Update the database to reflect the changes
-        cur.execute("UPDATE Customer SET username=? WHERE username=?", (username, self.__username))
-
-        # Commit the changes and close the connection
-        connect.commit()
-        connect.close()
-
-        self.__username = username
-
-    def setPassword(self, password):
-        # Establish connection with the db and create a cursor
-        connect = sqlite3.connect("e-commerce.db")
-        cur = connect.cursor()
-
-        # Update the database to reflect the changes
-        cur.execute("UPDATE Customer SET password=? WHERE username=?", (password, self.__username))
-
-        # Commit the changes and close the connection
-        connect.commit()
-        connect.close()
-
-        # Set the object's data to the passed-in value
-        self.__password = password
-
     def setAddress(self, address):
-        # Establish connection with the db and create a cursor
-        connect = sqlite3.connect("e-commerce.db")
-        cur = connect.cursor()
-
-        # Update the database to reflect the changes
-        cur.execute("UPDATE Customer SET address=? WHERE username=?", (address, self.__username))
-
-        # Commit the changes and close the connection
-        connect.commit()
-        connect.close()
-
-        # Set the object's data to the passed-in value
         self.__address = address
 
     def setCity(self, city):
-        # Establish connection with the db and create a cursor
-        connect = sqlite3.connect("e-commerce.db")
-        cur = connect.cursor()
-
-        # Update the database to reflect the changes
-        cur.execute("UPDATE Customer SET city=? WHERE username=?", (city, self.__username))
-
-        # Commit the changes and close the connection
-        connect.commit()
-        connect.close()
-
-        # Set the object's data to the passed-in value
         self.__city = city
 
-    def setState(self, state):
-        # Establish connection with the db and create a cursor
-        connect = sqlite3.connect("e-commerce.db")
-        cur = connect.cursor()
-
-        # Update the database to reflect the changes
-        cur.execute("UPDATE Customer SET state=? WHERE username=?", (state, self.__username))
-
-        # Commit the changes and close the connection
-        connect.commit()
-        connect.close()
-
-        # Set the object's data to the passed-in value
-        self.__state = state
-
-    def setZip(self, zipCode):
-        # Establish connection with the db and create a cursor
-        connect = sqlite3.connect("e-commerce.db")
-        cur = connect.cursor()
-
-        # Update the database to reflect the changes
-        cur.execute("UPDATE Customer SET zip=? WHERE username=?", (zipCode, self.__username))
-
-        # Commit the changes and close the connection
-        connect.commit()
-        connect.close()
-
-        # Set the object's data to the passed-in value
-        self.__zipCode = zipCode
+    def setZip(self, zip):
+        self.__zip = zip
 
     def setPaymentInfo(self, paymentInfo):
-        # Establish connection with the db and create a cursor
-        connect = sqlite3.connect("e-commerce.db")
-        cur = connect.cursor()
-
-        # Update the database to reflect the changes
-        cur.execute("UPDATE Customer SET paymentInfo=? WHERE username=?", (paymentInfo, self.__username))
-
-        # Commit the changes and close the connection
-        connect.commit()
-        connect.close()
-
-        # Set the object's data to the passed-in value
         self.__paymentInfo = paymentInfo
 
-    def getUsername(self):
-        return self.__username
+    def sedOrderHistory(self, orderHistory):
+        self.__orderHistory = orderHistory
 
-    def getPassword(self):
-        return self.__password
-
-    # * These are the getters and setters for the Customer class *
-
-    # This functions checks if the user entered valid credentials and returns the result
     def login(self):
         # Create a connection to the db and make a cursor
         connect = sqlite3.connect("e-commerce.db")
@@ -148,7 +47,6 @@ class Customer:
 
         return validated
 
-    # This function allows the user to create a set of login credentials in the database
     def create_account(self):
         # Create a connection to the db and make a cursor
         connect = sqlite3.connect("e-commerce.db")
@@ -168,9 +66,8 @@ class Customer:
 
         # If the username is not taken, push the data back and commit the changes
         if not_taken is True:
-            cur.execute("INSERT INTO Customer VALUES (?, ?, ?, ?, ?, ?, ?)", (self.__username, self.__password, None, None, None, None, None,))
-
-            # Commit the changes and close the connection
+            cur.execute(
+                "INSERT INTO Customer VALUES ('" + self.__username + "', " + "'" + self.__password + "', " + "'NULL', 'NULL', 'NULL', 'NULL', 'NULL')")
             connect.commit()
             result = True
         else:
@@ -179,23 +76,3 @@ class Customer:
         # Close the connection and return the result
         connect.close()
         return result
-
-    # Allows a user to delete their account
-    def delete_account(self):
-        # Create a connection to the database
-        connect = sqlite3.connect("e-commerce.db")
-        cur = connect.cursor()
-
-        # Attempt to delete the user from the db
-        cur.execute("DELETE FROM Customer WHERE username=?", (self.__username,))
-        result = cur.fetchall()
-
-        # Commit the changes and close the connection
-        connect.commit()
-        connect.close()
-
-        return
-
-    # Return a user's order history
-    def getOrderHistory(self):
-        pass
