b7a1c3515316e0aef469fc8cf6c7e1770f64b3fb
avuel <63319229+avuel@users.noreply.github.com>
2021-12-01 23:00:29 -0600

    Added ability to load inventory (#29)
    * changing inventory and add inventory db
    
    * Update Inventory.py
    
    * Added ability to load Invetory class and Database from the Items database
    
    * Update db.py
    
    Co-authored-by: Alonzo Reed Jr <ar2628@msstate.edu>

--- Inventory.py
+++ Inventory.py
@@ -29,3 +29,41 @@ class Inventory:
         print(f"The item {item} of the category {category} is not in the inventory")
         return None
    
+
+    def load_db(self) -> None:
+        import sqlite3 as sql
+        from sqlite3.dbapi2 import Connection, Cursor
+        from Item import Item
+
+        try:
+            # Connect to the database and create a cursor
+            conn: Connection = sql.connect('e-commerce.db')
+            c: Cursor = conn.cursor()
+
+            for item in self.__items:
+                current_item: Item = item
+                # Insert the item into our database (we do not need an itemID as it is automatically generated)
+                query: str = f"INSERT INTO items (itemID, stock) VALUES (:itemID, :stock)"
+                c.execute(query, {'itemID': current_item.getID(), 'stock': self.__items[item]})
+        
+        except sql.IntegrityError:
+            # If we get an integrity error, print that we failed to insert the item
+            print("Failed to insert an item into the inventory", end="\n\n")
+
+        # Commit our changes and close the database
+        conn.commit()
+        conn.close()
+            
+
+def load_inventory() -> Inventory:
+    from typing import Any
+    from Item import get_items
+
+    items: list[Any] = get_items()
+    inventory: Inventory = Inventory()
+    stock: int = 1
+    for item in items:
+        inventory.addItem(Item(item[0], item[1], item[2], item[3]), stock)
+        stock: int = stock + 1
+
+
