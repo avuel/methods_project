4e5101a7ed1ab1f6ad2d482ad27c6a9133392c24
Alonzo Reed Jr <ar2628@msstate.edu>
2021-12-01 22:25:34 -0600

    changing inventory and add inventory db

--- Inventory.py
+++ Inventory.py
@@ -1,18 +1,35 @@
+from Item import Item
+import sqlite3
+
 class Inventory:
     # Constructor
-    def __init__(self, Itemname: str) -> None:
-        self.Itemname = Itemname
+    def __init__(self) -> None:
+        self.__items = {}
+
+    # Change how the Inventory is printed
+    def __repr__(self) -> None:
+        repr(self.__items)
 
     # Add item to Inventory
-    def addItem(self, Itemname: str) ->None:
-        if self.Itemname != Itemname:
-            self.Itemname = Itemname
-        else:
-            print("Item Already in Inventory")
+    def addItem(self, new_item: Item, stock: int) -> bool:
+        new_name: str = new_item.getName()
+        new_category: str = new_item.getCategory()
+        for item in self.__items:
+            if item.getName() == new_name:
+                if item.getCategory() == new_category:
+                    print(f"The item {new_name} of the category {new_category} is already in the inventory")
+                    return None
+        self.__items[new_item] = stock
 
-    # Return an item (based on item name)
-    def getItem(self, name) -> str:
-        if self.Itemname == name:
-            return self.__Itemname
-        else:
-            print("Item is not in Inventory")
+    # Return an item (based on item name and category)
+    def getItem(self, name: str, category: str) -> Item:
+        for item in self.__items:
+            if item.getName() == name:
+                if item.getCategory() == category:
+                    return item
+        print(f"The item {item} of the category {category} is not in the inventory")
+        return None
+
+
+
+   
